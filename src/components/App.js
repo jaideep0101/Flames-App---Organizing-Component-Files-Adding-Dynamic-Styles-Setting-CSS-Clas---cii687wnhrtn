4e0e86508c0e1bf47f8e504017d6d9c679daf964
Jaideep singh thakur <88130081+jaideep0101@users.noreply.github.com>
2023-03-30 13:55:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,88 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [secondName, setSecondName] = useState("");
+  const [relationshipStatus, setRelationshipStatus] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+  const handleCalculateButtonClick = () => {
+    let firstString = firstName.trim().toLowerCase();
+    let secondString = secondName.trim().toLowerCase();
+
+    // Check for valid inputs
+    if (firstString === "" || secondString === "") {
+      setRelationshipStatus("Please Enter valid input");
+      return;
     }
+
+    // Remove common characters
+    for (let i = 0; i < firstString.length; i++) {
+      let currentChar = firstString.charAt(i);
+      if (secondString.includes(currentChar)) {
+        firstString = firstString.replace(currentChar, "");
+        secondString = secondString.replace(currentChar, "");
+        i--;
+      }
+    }
+
+    // Calculate relationship status
+    const lengthSum = firstString.length + secondString.length;
+    const status = lengthSum % 6;
+   
+switch (status) {
+      case 1:
+        setRelationshipStatus("Friends");
+        break;
+      case 2:
+        setRelationshipStatus("Love");
+        break;
+      case 3:
+        setRelationshipStatus("Affection");
+        break;
+      case 4:
+        setRelationshipStatus("Marriage");
+        break;
+      case 5:
+        setRelationshipStatus("Enemy");
+        break;
+      default:
+        setRelationshipStatus("Siblings");
+    }
+  };
+
+  const handleClearButtonClick = () => {
+    setFirstName("");
+    setSecondName("");
+   
+setRelationshipStatus("");
+  };
+
+  return (
+    <div id="main">
+      <input
+        type="text"
+        placeholder="Enter First Name"
+        data-testid="input1"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter Second Name"
+        data-testid="input2"
+        value={secondName}
+        onChange={(e) => setSecondName(e.target.value)}
+      />
+      <button data-testid="calculate_relationship" onClick={handleCalculateButtonClick}>
+        Calculate Relationship Future
+      </button>
+      <button data-testid="clear" onClick={handleClearButtonClick}>
+        Clear
+      </button>
+      <h3 data-testid="answer">{relationshipStatus}</h3>
+    </div>
+  );
 }
 
-
-export default App;
+export default App;
